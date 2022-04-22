b8d21e6262ecbb5a8b4dbf30a56f08ea47c9b54e
Erika Diana Guillén Leyva <erika@MacBook-Air-de-Erika.local>
2022-04-22 10:49:38 -0500

    Agregué área y perímetro

--- figuras.js
+++ figuras.js
@@ -1,1 +1,8 @@
-console.log("Hello world");
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado);
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perímetro del cuadrado es: " + perimetroCuadrado);
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El área del cuadrado es: " + areaCuadrado + "cm2");
