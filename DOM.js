4d2c74ea840a2ed3573fc8666db540f8f64ea1f3
Erika Guillén <id.eridian@gmail.com>
2023-09-25 21:41:33 -0600

    Práctica: Manipulación del DOM

---
+++ DOM.js
@@ -0,0 +1,20 @@
+const h1 = document.querySelector("h1");
+const p = document.querySelectorAll("p");
+const parrafoClase = document.querySelector(".parrafoClase");
+const parrafoID = document.getElementById("#parrafoID");
+const input = document.querySelector("input");
+
+console.log(input);
+
+console.log({
+  h1,
+  p,
+  parrafoClase,
+  parrafoID,
+  input,
+});
+
+console.log(h1);
+
+h1.innerHTML = "Patito <br> Feo";
+h;
